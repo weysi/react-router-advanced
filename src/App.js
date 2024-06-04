2f321020629d2cf8c9088bb8b303865aff476f08
weysi <uve.eng@outlook.com>
2024-06-04 23:55:47 +0200

    loader added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EventsPage from "./pages/Events";
@@ -25,7 +18,20 @@ const router = createBrowserRouter([
 				path: "events",
 				element: <EventsRootLayout />,
 				children: [
-					{ index: true, element: <EventsPage /> },
+					{
+						index: true,
+						element: <EventsPage />,
+						loader: async () => {
+							const response = await fetch("http://localhost:8080/events");
+
+							if (!response.ok) {
+								//...
+							} else {
+								const resData = await response.json();
+								return resData.events;
+							}
+						},
+					},
 					{ path: ":eventId", element: <EventDetailPage /> },
 					{ path: "new", element: <NewEventPage /> },
 					{ path: ":eventId/edit", element: <EditEventPage /> },
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,26 +1,11 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
-const DUMMY_EVENTS = [
-	{
-		id: "e1",
-		title: "Some event",
-	},
-	{
-		id: "e2",
-		title: "Another event",
-	},
-];
 function EventsPage() {
+	const events = useLoaderData();
 	return (
 		<>
-			<h1>EventsPage</h1>
-			<ul>
-				{DUMMY_EVENTS.map((event) => (
-					<li key={event.id}>
-						<Link to={event.id}>{event.title}</Link>
-					</li>
-				))}
-			</ul>
+			<EventsList events={events} />
 		</>
 	);
 }
