74380d10ed15942a1df16b7088f7528476b9a8e1
weysi <uve.eng@outlook.com>
2024-06-10 17:01:26 +0200

    edited

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -3,6 +3,8 @@ import {
 	useNavigation,
 	useNavigate,
 	useActionData,
+	json,
+	redirect,
 } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
@@ -18,7 +20,7 @@ function EventForm({ method, event }) {
 		navigate('..');
 	}
 	return (
-		<Form method='post' className={classes.form}>
+		<Form method={method} className={classes.form}>
 			{data && data.errors && (
 				<ul>
 					{Object.values(data.errors).map((err) => (
@@ -79,3 +81,38 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+	const method = request.method;
+	const data = await request.formData();
+
+	const eventData = {
+		title: data.get('title'),
+		image: data.get('image'),
+		date: data.get('date'),
+		description: data.get('description'),
+	};
+
+	console.log(eventData);
+	let url = 'http://localhost:8080/events';
+	if (method === 'PATCH') {
+		const eventId = params.eventId;
+		url = 'http://localhost:8080/events/' + eventId;
+	}
+
+	const response = await fetch(url, {
+		method: method,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(eventData),
+	});
+
+	if (response.status === 422) {
+		return response;
+	}
+	if (!response.ok) {
+		throw json({ message: 'Could not save event.' }, { status: 500 });
+	}
+	return redirect('/events');
+}
--- src/pages/EditEvent.js
+++ src/pages/EditEvent.js
@@ -1,10 +1,10 @@
-import { useRouteLoaderData } from "react-router-dom";
-import EventForm from "../components/EventForm";
+import { useRouteLoaderData } from 'react-router-dom';
+import EventForm from '../components/EventForm';
 
 function EditEventPage() {
-	const data = useRouteLoaderData("event-detail");
+	const data = useRouteLoaderData('event-detail');
 
-	return <EventForm event={data.event} />;
+	return <EventForm event={data.event} method='patch' />;
 }
 
 export default EditEventPage;
